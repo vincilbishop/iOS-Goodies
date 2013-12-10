63dfce9d46d39778c2f1274752cacca9aaae3a3d
Vincil Bishop <vincil.bishop@vbishop.com>
2013-12-09 20:41:27 -0600

    Renamed base picker controller

--- Classes/View/IOGBasePickerViewController.h
+++ Classes/View/IOGBasePickerViewController.h
@@ -1,14 +1,14 @@
 //
-//  VIBasePickerViewController.h
+//  IOGBasePickerViewController.h
 //  ValetIt
 //
 //  Created by Vincil Bishop on 12/3/13.
-//  Copyright (c) 2013 Valet-It. All rights reserved.
+//  Copyright (c) 2013 All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface VIBasePickerViewController : UIViewController
+@interface IOGBasePickerViewController : UIViewController
 
 @property (nonatomic,strong) NSArray *collection;
 @property (strong, nonatomic) IBOutlet UIToolbar *toolbar;
--- Classes/View/IOGBasePickerViewController.m
+++ Classes/View/IOGBasePickerViewController.m
@@ -1,20 +1,20 @@
 //
-//  VIBasePickerViewController.m
+//  IOGBasePickerViewController.m
 //  ValetIt
 //
 //  Created by Vincil Bishop on 12/3/13.
-//  Copyright (c) 2013 Valet-It. All rights reserved.
+//  Copyright (c) 2013 All rights reserved.
 //
 
-#import "VIBasePickerViewController.h"
+#import "IOGBasePickerViewController.h"
 
-@interface VIBasePickerViewController ()
+@interface IOGBasePickerViewController ()
 
 // TODO: Add blocks for done / cancel / etc. to make this super generic
 
 @end
 
-@implementation VIBasePickerViewController
+@implementation IOGBasePickerViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
