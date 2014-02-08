05efc59b79834b333a3273761491955a9caa1ee5
Vincil Bishop <vincil.bishop@vbishop.com>
2014-02-07 20:17:00 -0600

    Updte iOS goodies with Location manager ,bluetooth manager

---
+++ Classes/Controller/CoreBluetooth/IOGPeripheralManager.h
@@ -0,0 +1,13 @@
+//
+//  IOGPeripheralManager.h
+//  Pods
+//
+//  Created by Vincil Bishop on 2/7/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IOGPeripheralManager : CBPeripheralManager
+
+@end
---
+++ Classes/Controller/CoreBluetooth/IOGPeripheralManager.m
@@ -0,0 +1,13 @@
+//
+//  IOGPeripheralManager.m
+//  Pods
+//
+//  Created by Vincil Bishop on 2/7/14.
+//
+//
+
+#import "IOGPeripheralManager.h"
+
+@implementation IOGPeripheralManager
+
+@end
